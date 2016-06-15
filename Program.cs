1b0a34c9646ba183ea285ce21f8c9683ab2bdf8f
banhtieu <tuoitrevohoc@me.com>
2016-06-15 12:27:32 +0700

    listen to external ip

--- Program.cs
+++ Program.cs
@@ -13,11 +13,11 @@ namespace SwiftChallenge
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000")
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000")
                 .Build();
 
             host.Run();
