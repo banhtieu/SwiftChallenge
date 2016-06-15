4700c756fe98c8cb46b0fd08b0174e8239465090
banhtieu <tuoitrevohoc@me.com>
2016-06-15 19:06:18 +0700

    + Add files.

---
+++ wwwroot/app/application.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    moduleId: module.id,
+    selector: 'the-application',
+    templateUrl: 'application.component.html'
+})
+export class ApplicationComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
---
+++ wwwroot/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var application_component_1 = require('./application.component');
+platform_browser_dynamic_1.bootstrap(application_component_1.ApplicationComponent);
+//# sourceMappingURL=main.js.map
